ede6a4a6c5bec19f1388259814eacad864fd13e2
Aleksander Jaworski <86464252+aleck156@users.noreply.github.com>
2026-02-26 07:18:46 +0100

    Create db_init.sql

---
+++ scripts/db_init.sql
@@ -0,0 +1,27 @@
+USE master;
+
+-- Drop and recreate the DataWarehouse DB
+
+IF EXISTS(SELECT 1 FROM sys.databases WHERE name='DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse
+END;
+GO
+
+-- Create the 'DataWarehouse' DB
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
