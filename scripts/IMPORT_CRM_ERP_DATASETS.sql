83917e76b2ad72e478cb3942757b7781e5fb33f7
Aleksander Jaworski <86464252+aleck156@users.noreply.github.com>
2026-02-27 17:14:19 +0100

    Update IMPORT_CRM_ERP_DATASETS.sql
    Add error handling during data import.

--- scripts/IMPORT_CRM_ERP_DATASETS.sql
+++ scripts/IMPORT_CRM_ERP_DATASETS.sql
@@ -1,103 +1,112 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	PRINT '------------------------------------------------------------------------'
-	PRINT '| Loading Bronze Layer data sets from CRM and ERP systems via csv files'
-	PRINT '------------------------------------------------------------------------'
+	BEGIN TRY
+		PRINT '------------------------------------------------------------------------'
+		PRINT '| Loading Bronze Layer data sets from CRM and ERP systems via csv files'
+		PRINT '------------------------------------------------------------------------'
 
-	PRINT '| 1/2 Loading CRM tables'	
-	PRINT '------------------------------------------------------------------------'
-	PRINT '>> Truncating Table: bronze.crm_cust_info'
+		PRINT '| 1/2 Loading CRM tables'	
+		PRINT '------------------------------------------------------------------------'
+		PRINT '>> Truncating Table: bronze.crm_cust_info'
 	
-	-- Truncate bronze.crm_cust_info before importing any data
-	TRUNCATE TABLE bronze.crm_cust_info
+		-- Truncate bronze.crm_cust_info before importing any data
+		TRUNCATE TABLE bronze.crm_cust_info
 
-	-- BULK IMPORT operation for CRM system and cust_info.csv
-	PRINT '>> Inserting Data into Table: bronze.crm_cust_info'
-	BULK INSERT bronze.crm_cust_info
-	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		-- BULK IMPORT operation for CRM system and cust_info.csv
+		PRINT '>> Inserting Data into Table: bronze.crm_cust_info'
+		BULK INSERT bronze.crm_cust_info
+		FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '------------------------------------------------------------------------'
-	PRINT '>> Truncating Table: bronze.crm_prd_info'
-	-- Truncate bronze.crm_prd_info before importing any data
-	TRUNCATE TABLE bronze.crm_prd_info
+		PRINT '------------------------------------------------------------------------'
+		PRINT '>> Truncating Table: bronze.crm_prd_info'
+		-- Truncate bronze.crm_prd_info before importing any data
+		TRUNCATE TABLE bronze.crm_prd_info
 
-	-- BULK IMPORT operation for CRM system and prd_info.csv
+		-- BULK IMPORT operation for CRM system and prd_info.csv
 	
-	PRINT '>> Inserting Data into Table: bronze.crm_prd_info'
-	BULK INSERT bronze.crm_prd_info
-	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Inserting Data into Table: bronze.crm_prd_info'
+		BULK INSERT bronze.crm_prd_info
+		FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '------------------------------------------------------------------------'
-	PRINT '>> Truncating Table: bronze.crm_sales_details'
-	-- Truncate bronze.crm_sales_details before importing any data
-	TRUNCATE TABLE bronze.crm_sales_details
+		PRINT '------------------------------------------------------------------------'
+		PRINT '>> Truncating Table: bronze.crm_sales_details'
+		-- Truncate bronze.crm_sales_details before importing any data
+		TRUNCATE TABLE bronze.crm_sales_details
 
-	-- BULK IMPORT operation for CRM system and sales_details.csv	
-	PRINT '>> Inserting Data into Table: bronze.crm_sales_details'
-	BULK INSERT bronze.crm_sales_details
-	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		-- BULK IMPORT operation for CRM system and sales_details.csv	
+		PRINT '>> Inserting Data into Table: bronze.crm_sales_details'
+		BULK INSERT bronze.crm_sales_details
+		FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
 	
-	PRINT '------------------------------------------------------------------------'
-	PRINT '| 2/2 Loading ERP tables'	
-	PRINT '------------------------------------------------------------------------'
-	PRINT '>> Truncating Table: bronze.erp_CUST_AZ12'
-	-- Truncate bronze.erp_CUST_AZ12 before importing any data
-	TRUNCATE TABLE bronze.erp_CUST_AZ12
+		PRINT '------------------------------------------------------------------------'
+		PRINT '| 2/2 Loading ERP tables'	
+		PRINT '------------------------------------------------------------------------'
+		PRINT '>> Truncating Table: bronze.erp_CUST_AZ12'
+		-- Truncate bronze.erp_CUST_AZ12 before importing any data
+		TRUNCATE TABLE bronze.erp_CUST_AZ12
 
-	-- BULK IMPORT operation for ERP system and sales_details.csv	
-	PRINT '>> Inserting Data into Table: bronze.erp_CUST_AZ12'
-	BULK INSERT bronze.erp_CUST_AZ12
-	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		-- BULK IMPORT operation for ERP system and sales_details.csv	
+		PRINT '>> Inserting Data into Table: bronze.erp_CUST_AZ12'
+		BULK INSERT bronze.erp_CUST_AZ12
+		FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '------------------------------------------------------------------------'
-	PRINT '>> Truncating Table: bronze.erp_LOC_A101'
-	-- Truncate bronze.erp_LOC_A101 before importing any data
-	TRUNCATE TABLE bronze.erp_LOC_A101
+		PRINT '------------------------------------------------------------------------'
+		PRINT '>> Truncating Table: bronze.erp_LOC_A101'
+		-- Truncate bronze.erp_LOC_A101 before importing any data
+		TRUNCATE TABLE bronze.erp_LOC_A101
 
-	-- BULK IMPORT operation for ERP system and LOC_A101.csv
-	PRINT '>> Inserting Data into Table: bronze.erp_LOC_A101'
-	BULK INSERT bronze.erp_LOC_A101
-	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		-- BULK IMPORT operation for ERP system and LOC_A101.csv
+		PRINT '>> Inserting Data into Table: bronze.erp_LOC_A101'
+		BULK INSERT bronze.erp_LOC_A101
+		FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '------------------------------------------------------------------------'
-	PRINT '>> Truncating Table: bronze.erp_PX_CAT_G1V2'
-	-- Truncate bronze.erp_PX_CAT_G1V2 before importing any data
-	TRUNCATE TABLE bronze.erp_PX_CAT_G1V2
+		PRINT '------------------------------------------------------------------------'
+		PRINT '>> Truncating Table: bronze.erp_PX_CAT_G1V2'
+		-- Truncate bronze.erp_PX_CAT_G1V2 before importing any data
+		TRUNCATE TABLE bronze.erp_PX_CAT_G1V2
 
-	-- BULK IMPORT operation for ERP system and PX_CAT_G1V2.csv
-	PRINT '>> Inserting Data into Table: bronze.erp_PX_CAT_G1V2'
-	BULK INSERT bronze.erp_PX_CAT_G1V2
-	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
-
+		-- BULK IMPORT operation for ERP system and PX_CAT_G1V2.csv
+		PRINT '>> Inserting Data into Table: bronze.erp_PX_CAT_G1V2'
+		BULK INSERT bronze.erp_PX_CAT_G1V2
+		FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		END TRY
+	BEGIN CATCH
+		PRINT '------------------------------------------------------------------------'
+		PRINT '| Error occured during loading bronze layer'
+		PRINT '| Error Message: ' + ERROR_MESSAGE()
+		PRINT '| Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR)
+		PRINT '| Error State: ' + CAST(ERROR_STATE() AS NVARCHAR)
+		PRINT '------------------------------------------------------------------------'
+	END CATCH
 END
