b2f5ce3239efb6131504b20f45dcfc8997b207c4
Aleksander Jaworski <86464252+aleck156@users.noreply.github.com>
2026-02-27 17:05:51 +0100

    Update IMPORT_CRM_ERP_DATASETS.sql
    Output formatting improved for clarity and human readability.

--- scripts/IMPORT_CRM_ERP_DATASETS.sql
+++ scripts/IMPORT_CRM_ERP_DATASETS.sql
@@ -1,10 +1,18 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+	PRINT '------------------------------------------------------------------------'
+	PRINT '| Loading Bronze Layer data sets from CRM and ERP systems via csv files'
+	PRINT '------------------------------------------------------------------------'
 
+	PRINT '| 1/2 Loading CRM tables'	
+	PRINT '------------------------------------------------------------------------'
+	PRINT '>> Truncating Table: bronze.crm_cust_info'
+	
 	-- Truncate bronze.crm_cust_info before importing any data
 	TRUNCATE TABLE bronze.crm_cust_info
 
 	-- BULK IMPORT operation for CRM system and cust_info.csv
+	PRINT '>> Inserting Data into Table: bronze.crm_cust_info'
 	BULK INSERT bronze.crm_cust_info
 	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 	WITH (
@@ -13,10 +21,14 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '------------------------------------------------------------------------'
+	PRINT '>> Truncating Table: bronze.crm_prd_info'
 	-- Truncate bronze.crm_prd_info before importing any data
 	TRUNCATE TABLE bronze.crm_prd_info
 
 	-- BULK IMPORT operation for CRM system and prd_info.csv
+	
+	PRINT '>> Inserting Data into Table: bronze.crm_prd_info'
 	BULK INSERT bronze.crm_prd_info
 	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 	WITH (
@@ -25,10 +37,13 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '------------------------------------------------------------------------'
+	PRINT '>> Truncating Table: bronze.crm_sales_details'
 	-- Truncate bronze.crm_sales_details before importing any data
 	TRUNCATE TABLE bronze.crm_sales_details
 
-	-- BULK IMPORT operation for CRM system and sales_details.csv
+	-- BULK IMPORT operation for CRM system and sales_details.csv	
+	PRINT '>> Inserting Data into Table: bronze.crm_sales_details'
 	BULK INSERT bronze.crm_sales_details
 	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 	WITH (
@@ -37,10 +52,16 @@ BEGIN
 		TABLOCK
 	);
 
+	
+	PRINT '------------------------------------------------------------------------'
+	PRINT '| 2/2 Loading ERP tables'	
+	PRINT '------------------------------------------------------------------------'
+	PRINT '>> Truncating Table: bronze.erp_CUST_AZ12'
 	-- Truncate bronze.erp_CUST_AZ12 before importing any data
 	TRUNCATE TABLE bronze.erp_CUST_AZ12
 
-	-- BULK IMPORT operation for ERP system and sales_details.csv
+	-- BULK IMPORT operation for ERP system and sales_details.csv	
+	PRINT '>> Inserting Data into Table: bronze.erp_CUST_AZ12'
 	BULK INSERT bronze.erp_CUST_AZ12
 	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 	WITH (
@@ -49,10 +70,13 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '------------------------------------------------------------------------'
+	PRINT '>> Truncating Table: bronze.erp_LOC_A101'
 	-- Truncate bronze.erp_LOC_A101 before importing any data
 	TRUNCATE TABLE bronze.erp_LOC_A101
 
 	-- BULK IMPORT operation for ERP system and LOC_A101.csv
+	PRINT '>> Inserting Data into Table: bronze.erp_LOC_A101'
 	BULK INSERT bronze.erp_LOC_A101
 	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 	WITH (
@@ -61,10 +85,13 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '------------------------------------------------------------------------'
+	PRINT '>> Truncating Table: bronze.erp_PX_CAT_G1V2'
 	-- Truncate bronze.erp_PX_CAT_G1V2 before importing any data
 	TRUNCATE TABLE bronze.erp_PX_CAT_G1V2
 
 	-- BULK IMPORT operation for ERP system and PX_CAT_G1V2.csv
+	PRINT '>> Inserting Data into Table: bronze.erp_PX_CAT_G1V2'
 	BULK INSERT bronze.erp_PX_CAT_G1V2
 	FROM 'Y:\[SQL]\[SQL_DATA_WAREHOUSE]\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 	WITH (
