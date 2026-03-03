edf87e4582c10072cc7f0167afe3ee801b75b029
Aleksander Jaworski <86464252+aleck156@users.noreply.github.com>
2026-03-04 00:11:02 +0100

    Create DDL_DML_silver.sql

---
+++ scripts/silver/DDL_DML_silver.sql
@@ -0,0 +1,353 @@
+/*
+DML
+	- Cleaning the data before importing into target tables
+*/
+
+-- ###############################################################################
+-- TABLE:	silver.crm_cust_info
+-- ###############################################################################
+-- CREATE TABLE
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_cust_info
+
+CREATE TABLE silver.crm_cust_info (
+	cst_id					INT,
+	cst_key					NVARCHAR(50),
+	cst_firstname			NVARCHAR(50),
+	cst_lastname			NVARCHAR(50),
+	cst_marital_status		NVARCHAR(50),
+	cst_gndr				NVARCHAR(50),
+	cst_create_date			DATE,
+	dwh_create_date			DATETIME2 DEFAULT GETDATE()
+);
+
+-- INSERT DATA
+INSERT INTO silver.crm_cust_info (
+	cst_id
+	, cst_key
+	, cst_firstname
+	, cst_lastname
+	, cst_marital_status
+	, cst_gndr
+	, cst_create_date
+) 
+
+SELECT
+	cst_id
+	, cst_key
+	, TRIM(cst_firstname) AS cst_firstname
+	, TRIM(cst_lastname) AS cst_lastname
+	, CASE
+			WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+			WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+			ELSE 'n/a'
+		END AS cst_marital_status
+		-- TRIM(cst_marital_status) cst_marital_status
+	, CASE 
+			WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+			WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+			ELSE 'n/a'
+		END AS cst_gndr
+	, cst_create_date
+FROM (
+	SELECT 
+		*
+		, ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+) t WHERE flag_last = 1
+
+-- ###############################################################################
+-- TABLE:	silver.crm_prd_info
+-- ###############################################################################
+-- CREATE TABLE
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info
+
+CREATE TABLE silver.crm_prd_info(
+	prd_id				INT,
+	cat_id				NVARCHAR(50),
+	prd_key				NVARCHAR(50),
+	prd_nm				NVARCHAR(50),
+	prd_cost			INT,
+	prd_line			NVARCHAR(50),
+	prd_start_dt		DATE,
+	prd_end_dt			DATE,
+	dwh_create_date			DATETIME2 DEFAULT GETDATE()
+);
+
+-- INSERT DATA
+INSERT INTO silver.crm_prd_info (
+	prd_id
+	, cat_id
+	, prd_key
+	, prd_nm
+	, prd_cost
+	, prd_line
+	, prd_start_dt
+	, prd_end_dt
+)
+SELECT
+	prd_id
+	, REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id
+	, SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key
+	, prd_nm
+	, ISNULL(prd_cost, 0) AS prd_cost
+	, CASE UPPER(TRIM(prd_line))
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'		
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END AS prd_line
+	, CAST(prd_start_dt AS DATE) AS prd_start_dt
+	, CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_start_dt_UPDATED
+FROM bronze.crm_prd_info
+
+-- VALIDATE RESULTS
+-- Check for NULLs or Duplicates in Primary Key
+SELECT
+	prd_id
+	, COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+-- Check for unwanted spaces
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+-- Check for Nulls or negative numbers
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR  prd_cost IS NULL
+
+-- Data Standardization & Consistency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+-- Check for invalid Date Orders
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_start_dt > prd_end_dt
+	OR prd_start_dt IS NULL
+
+-- Final look on the data
+SELECT *
+FROM silver.crm_prd_info
+
+-- ###############################################################################
+-- TABLE:	silver.crm_sales_details
+-- ###############################################################################
+-- CREATE TABLE
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details
+
+CREATE TABLE silver.crm_sales_details(
+	sls_ord_num			NVARCHAR(50),
+	sls_prd_key			NVARCHAR(50),
+	sls_cust_id			INT,
+	sls_order_dt		DATE,
+	sls_ship_dt			DATE,
+	sls_due_dt			DATE,
+	sls_sales			INT,
+	sls_quantity		INT,
+	sls_price			INT,
+	dwh_create_date			DATETIME2 DEFAULT GETDATE()
+);
+
+-- INSERT DATA
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num
+	, sls_prd_key
+	, sls_cust_id
+	, sls_order_dt
+	, sls_ship_dt
+	, sls_due_dt
+	, sls_sales
+	, sls_quantity
+	, sls_price
+)
+
+SELECT
+	sls_ord_num
+	, sls_prd_key
+	, sls_cust_id
+	, CASE 
+		WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END AS sls_order_dt
+	, CASE 
+		WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END AS sls_ship_dt
+	, CASE 
+		WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END AS sls_due_dt
+	, CASE -- BUILDING NEW sls_sales column with updated rules
+		WHEN sls_sales <= 0 OR sls_sales IS NULL OR sls_sales != sls_quantity * ABS(sls_price)
+			THEN ABS(sls_quantity * sls_price)
+		ELSE sls_sales
+	END AS sls_sales
+	, sls_quantity
+	, CASE
+		WHEN sls_price IS NULL OR sls_price <=0 THEN sls_sales / NULLIF(sls_quantity, 0)
+		ELSE sls_price 
+	END AS sls_price
+FROM bronze.crm_sales_details
+
+-- VALIDATE RESULTS
+-- Check for Invalid Date Orders
+SELECT *
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+-- Check Data Consistency: Between Sales, Quantity and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero or negative
+SELECT *
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+	OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+	OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0 
+
+-- Final look on the data
+SELECT *
+FROM silver.crm_sales_details
+
+-- ###############################################################################
+-- TABLE:	silver.erp_CUST_AZ12
+-- ###############################################################################
+-- CREATE TABLE
+IF OBJECT_ID('silver.erp_CUST_AZ12', 'U') IS NOT NULL
+	DROP TABLE silver.erp_CUST_AZ12
+
+CREATE TABLE silver.erp_CUST_AZ12(
+	CID					NVARCHAR(50),
+	BDATE				DATE,
+	GEN					NVARCHAR(50),
+	dwh_create_date			DATETIME2 DEFAULT GETDATE()
+);
+
+-- INSERT DATA
+INSERT INTO silver.erp_CUST_AZ12(
+	cid
+	, BDATE
+	, GEN
+)
+
+SELECT
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)-3)
+		ELSE cid
+	END AS cid
+	, CASE
+		WHEN BDATE > GETDATE() THEN NULL
+		ELSE BDATE
+	END AS BDATE
+	, CASE 
+		WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+		WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+		ELSE 'n/a'
+	END AS GEN
+FROM bronze.erp_CUST_AZ12
+
+-- VALIDATE RESULTS
+-- Identify Out-Of-Range Dates
+SELECT BDATE
+FROM silver.erp_CUST_AZ12
+WHERE BDATE > GETDATE()
+
+-- Data Standardization & Consistency
+SELECT DISTINCT gen
+FROM silver.erp_CUST_AZ12
+
+-- Final look on the data
+SELECT *
+FROM silver.erp_CUST_AZ12
+
+-- ###############################################################################
+-- TABLE:	silver.erp_LOC_A101
+-- ###############################################################################
+-- CREATE TABLE
+IF OBJECT_ID('silver.erp_LOC_A101', 'U') IS NOT NULL
+	DROP TABLE silver.erp_LOC_A101
+
+CREATE TABLE silver.erp_LOC_A101(
+	CID					NVARCHAR(50),
+	CNTRY				NVARCHAR(50),
+	dwh_create_date			DATETIME2 DEFAULT GETDATE()
+);
+
+-- INSERT DATA
+INSERT INTO silver.erp_LOC_A101
+(
+	cid
+	, CNTRY
+)
+
+SELECT
+	REPLACE(cid, '-', '') AS cid
+	, CASE
+		WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
+		WHEN TRIM(CNTRY) IN ('US', 'USA') THEN 'United States'
+		WHEN TRIM(CNTRY) = '' OR CNTRY IS NULL THEN 'n/a'
+		ELSE TRIM(CNTRY)
+	END AS CNTRY
+FROM bronze.erp_LOC_A101
+
+-- VALIDATE RESULTS
+-- Invalid CID keys
+SELECT
+	cid
+FROM silver.erp_LOC_A101
+WHERE cid NOT IN (
+	SELECT
+		cst_key
+	FROM silver.crm_cust_info
+)
+
+-- Data Standardization & Consistency - CNTRY column
+SELECT DISTINCT CNTRY
+FROM silver.erp_LOC_A101
+ORDER BY CNTRY 
+
+-- Final look on the data
+SELECT *
+FROM silver.erp_LOC_A101
+
+-- ###############################################################################
+-- TABLE:	silver.erp_PX_CAT_G1V2
+-- ###############################################################################
+-- CREATE TABLE
+IF OBJECT_ID('silver.erp_PX_CAT_G1V2', 'U') IS NOT NULL
+	DROP TABLE silver.erp_PX_CAT_G1V2
+
+CREATE TABLE silver.erp_PX_CAT_G1V2(
+	ID					NVARCHAR(50),
+	CAT					NVARCHAR(50),
+	SUBCAT				NVARCHAR(50),
+	MAINTENANCE			NVARCHAR(50),
+	dwh_create_date			DATETIME2 DEFAULT GETDATE()
+);
+
+-- INSERT DATA
+INSERT INTO silver.erp_PX_CAT_G1V2(
+	ID
+	, CAT
+	, SUBCAT
+	, MAINTENANCE
+)
+
+SELECT
+	ID
+	, CAT
+	, SUBCAT
+	, MAINTENANCE
+FROM bronze.erp_PX_CAT_G1V2
+
+-- VALIDATE RESULTS
+-- Final look on the data
+SELECT *
+FROM silver.erp_PX_CAT_G1V2
