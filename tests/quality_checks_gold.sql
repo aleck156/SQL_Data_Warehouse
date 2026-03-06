a6369811ae66cb81aa23e973febda03b108f24b3
Aleksander Jaworski <86464252+aleck156@users.noreply.github.com>
2026-03-07 00:40:17 +0100

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,1 +1,15 @@
+-- #####################################################################
+-- Checking gold.dim_customers
+-- #####################################################################
+-- Check for uniqueness of Customer Key in gold.dim_customers
+-- Expected result: No rows
+SELECT
+    customer_key
+    , COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1
+
+
+
 
