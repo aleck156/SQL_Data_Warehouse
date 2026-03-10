61232273de4e44bd652f6524e045f727bbf9286d
Aleksander Jaworski <86464252+aleck156@users.noreply.github.com>
2026-03-10 17:20:09 +0100

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,1 +1,30 @@
+-- #####################################################################
+-- Checking silver.crm_cust_info
+-- #####################################################################
+-- Check for NULLs or Duplicates in Primary Key
+-- Expected result: No Results
+SELECT
+  cst_id
+  , COUNT(*)
+FROM silver.crm_cust_id
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- Check for Unwanted Spaces
+-- Expected result: No Results
+SELECT
+  cst_key
+FROM silver.crm_cust_info
+WHERE cst_key != TRIM(cst_key);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT
+  cst_marital_status
+FROM silver.crm_cust_info;
+
+-- #####################################################################
+-- Checking 
+-- #####################################################################
+-- Check for uniqueness of Product Key in gold.dim_products
+-- Expected result: No Results
 
