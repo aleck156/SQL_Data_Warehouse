57d852f77a1488fe2ed7f0d9fa0d92d701d430a5
Aleksander Jaworski <86464252+aleck156@users.noreply.github.com>
2026-03-10 17:41:45 +0100

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -53,7 +53,7 @@ WHERE prd_cost < 0 OR prd_cost IS NULL;
 SELECT
   *
 FROM silver.crm_prd_info
-WHERE prd_end_dt < prd_start_date
+WHERE prd_end_dt < prd_start_date;
 
 -- Data Standardization & Consistency
 SELECT DISTINCT
@@ -79,7 +79,7 @@ SELECT
   *
 FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt
-  OR sls_order_dt > sls_due_dt
+  OR sls_order_dt > sls_due_dt;
 
 -- Check Data Consistency: Sales = Quantity * Price
 -- Expected result: No Results
@@ -96,6 +96,48 @@ WHERE sls_sales != sls_quantity * sls_price
   OR sls_quantity <= 0
   OR sls_pricE <= 0
 ORDER BY sls_sales, sls_quantity, sls_price;
+
+-- #####################################################################
+-- Checking silver.erp_cust_az12
+-- #####################################################################
+-- Identify Out-Of-Range Dates
+-- Expected result: Birthdates between 1924-01-01 and Today
+SELECT DISTINCT
+  bdate
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01'
+  OR bdate > GETDATE();
+
+-- Data Standardization & Consistency
+SELECT DISTINCT
+  gen
+FROM silver.erp_cust_az12;
+
+-- #####################################################################
+-- Checking silver.erp_loc_a101
+-- #####################################################################
+-- Data Standardization & Consistency
+SELECT DISTINCT
+  cntry
+FROM silver.erp_loc_a101
+ORDER BY cntry;
+
+-- #####################################################################
+-- Checking silver.erp_px_cat_g1v2
+-- #####################################################################
+-- Check for Unwanted Spaces
+-- Expected result: No Results
+SELECT
+  *
+FROM silver.erp_px_cat_g1v2
+WHERE cat != TRIM(CAT)
+  OR subcat != TRIM(subcat)
+  OR maintenance != TRIM(maintenance);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT
+  maintenance
+FROM silver.erp_px_cat_g1v2;
 
 
 
