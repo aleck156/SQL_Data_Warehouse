75d9f80d225caaef5a8bdb03cb56708db5428f82
Aleksander Jaworski <86464252+aleck156@users.noreply.github.com>
2026-03-10 17:27:37 +0100

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -23,8 +23,40 @@ SELECT DISTINCT
 FROM silver.crm_cust_info;
 
 -- #####################################################################
--- Checking 
+-- Checking silver.crm_prd_info
 -- #####################################################################
--- Check for uniqueness of Product Key in gold.dim_products
+-- Check for NULLs or Duplicates in Primary Key
 -- Expected result: No Results
+SELECT
+  prd_id
+  , COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for Unwanted Spaces
+-- Expected result: No Results
+SELECT
+  prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or NEgative Values in cost
+-- Expected result: No Results
+SELECT
+  prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Check for Invalid Date Orders ( Start Date > End Date)
+-- Expected result: no Results
+SELECT
+  *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_date
+
+-- Data Standardization & Consistency
+SELECT DISTINCT
+  prd_line
+FROM silver.crm_prd_info;
 
